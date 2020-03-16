e6cc5c601b5b521790f05935f536ff725d1544df
Ivan Pogorelov <boa88@bk.ru>
2020-03-16 22:17:49 +0300

    Исправление структуры бд

--- public/migrate.php
+++ public/migrate.php
@@ -9,13 +9,13 @@ $dbh = new PDO($config['db_dsn'], $config['db_user'], $config['db_password']);
 
 $dbh->query("CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(32) COLLATE utf8_unicode_ci,
+  `email` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
   `name` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `photo` varchar(64) COLLATE utf8_unicode_ci NOT NULL DEFAULT ''
+  `photo` varchar(64) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 ");
